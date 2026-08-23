6816f1d53ac3d947454312b3e845999eb5e0142e
Derin Eryilmaz <derineryilmaz0@gmail.com>
2026-08-23 11:30:27 -0400

    vercel it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   integrations: [mdx()],
+  adapter: vercel(),
 });
